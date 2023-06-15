baea073329dac23e11c050b7472341d19aa69e12
lsy3709 <lsy3709@naver.com>
2023-06-15 11:52:17 +0900

    검증,페이징 작업중

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,18 @@
 import Post from '../../models/post.js';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  return next();
+};
+
 /*
 게시글 샘플 
 {
@@ -8,6 +22,21 @@ import Post from '../../models/post.js';
 }
 */
 export const write = async (ctx) => {
+  //검증 추가
+  const schema = Joi.object().keys({
+    title: Joi.string().required(), // required() 있으면 필수 검사 항목
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  });
+
+  //실패시
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
@@ -24,7 +53,7 @@ export const write = async (ctx) => {
 
 export const list = async (ctx) => {
   try {
-    const posts = await Post.find().exec();
+    const posts = await Post.find().sort({ _id: -1 }).limit(10).exec();
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e);
@@ -45,6 +74,55 @@ export const read = async (ctx) => {
   }
 };
 
-export const remove = (ctx) => {};
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // no content , 성공이지만 응답데이터없음
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
-export const update = (ctx) => {};
+/*
+PATCH /api/posts/:id 
+{
+    title:'수정',
+    body: '수정 내용',
+    tags: ['수정','태그']
+}
+*/
+
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+
+  //검증.
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  //실패시
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      // 이 값이 설정해야 업데이트된 데이터를 반환.
+      // false , 업데이트 전 데이터 반환.
+      new: true,
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -7,6 +7,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api/index.js';
+import createFakeData from './createFakeData.js';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -14,6 +15,7 @@ mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
+    //createFakeData();
   })
   .catch((e) => {
     console.log(e);
