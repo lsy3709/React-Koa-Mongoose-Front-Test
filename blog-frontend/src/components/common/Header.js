fde15c2d5534d075518996874d9ae673f512e780
lsy3709 <lsy3709@naver.com>
2023-06-20 14:45:30 +0900

    글쓰기 Quill 에디터 확인

--- blog-frontend/src/components/common/Header.js
+++ blog-frontend/src/components/common/Header.js
@@ -32,7 +32,7 @@ const UserInfo = styled.div`
   font-weight: 800;
   margin-right: 1rem;
 `;
-const Header = ({ user }) => {
+const Header = ({ user, onLogout }) => {
   return (
     <>
       <HeaderBlock>
@@ -43,7 +43,7 @@ const Header = ({ user }) => {
           {user ? (
             <div className="right">
               <UserInfo>{user.username}</UserInfo>
-              <Button>로그아웃</Button>
+              <Button onClick={onLogout}>로그아웃</Button>
             </div>
           ) : (
             <div className="right">
