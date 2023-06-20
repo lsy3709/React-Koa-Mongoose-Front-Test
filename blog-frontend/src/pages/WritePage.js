8b112e1f9c9a1e548fb69c87f358175b33edb9b1
lsy3709 <lsy3709@naver.com>
2023-06-20 16:34:23 +0900

    글쓰기 UI 확인

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,12 +1,14 @@
 import Editor from '../components/write/Editor';
 import Responsive from '../components/common/Responsive';
 import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 };
