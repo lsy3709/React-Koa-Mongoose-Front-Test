f7413f4db50fbf84137ee8840d9a328a5df38086
lsy3709 <lsy3709@naver.com>
2023-06-22 08:58:29 +0900

    포스트 리스트 작업중

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostList from '../components/posts/PostList';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <div>Hello React</div>
+      <PostList />
     </>
   );
 };
