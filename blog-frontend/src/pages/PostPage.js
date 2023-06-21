782aa293005fdf13935c1598356db743f80734d1
lsy3709 <lsy3709@naver.com>
2023-06-21 14:42:13 +0900

    목록 리스트 UI 확인.

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostViewr from '../components/post/PostViewr';
 import { useParams } from 'react-router-dom';
 
 const PostPage = () => {
   const { username, postId } = useParams();
   return (
-    <div>
-      <h1>포스트 읽기</h1>
-      <h4>username : {username}</h4>
-      <h4>postId : {postId}</h4>
-    </div>
+    <>
+      <HeaderContainer />
+      <PostViewr />
+    </>
   );
 };
 
